c94e7a43f044d47c0eabbe419827569fc03e2e02
Daniel33700 <danizel.on@gmail.com>
2025-01-02 15:39:08 +0100

    Remove login functionality

--- lib/views/welcome.dart
+++ lib/views/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:to_bee/views/login_screen.dart';
-import 'package:to_bee/views/sign_up.dart';
+import 'package:to_bee/views/home.dart';
+//import 'package:to_bee/views/login_screen.dart';
+//import 'package:to_bee/views/sign_up.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
@@ -13,140 +14,127 @@ class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(height: 20,),
-          Container(width:302,height:309.51,
-      child: Image.asset("lib/assets/images/Group25.png",fit:BoxFit.fill,))
-,
+          SizedBox(height: 20),
           Container(
-            width:430,height:378,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),color: Color(0xffFFD3B2)),
+            width: 302,
+            height: 309.51,
+            child: Image.asset(
+              "lib/assets/images/Group25.png",
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            width: 430,
+            height: 378,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color(0xffFFD3B2)),
             child: Column(
-          children: [
-            SizedBox(height:50),
-            Text("Welcome",style:TextStyle(color:Color(0xff333333),fontSize:20,fontWeight: FontWeight.bold )),
-            SizedBox(height:20),
-
-
-
-    Container(
-    width: 365,
-    height:108,
-
-
-    child: Column(
-    children: [
-    Text("ToDo helps you organize ",style:TextStyle(color:Color(0xff707070),fontSize:18.5,fontWeight: FontWeight.bold)),
-
-    Text("your day and achive your",style:TextStyle(color:Color(0xff707070),fontSize:18.5,fontWeight: FontWeight.bold)),
-
-    Text("goals effortlessly.",style:TextStyle(color:Color(0xff707070),fontSize:18.5,fontWeight: FontWeight.bold)),
-
-    ],
-    ))
-        ,Padding(
-      padding: const EdgeInsets.all(20),
-      child: Container(
-
-          child: Row (
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children:[
-      SizedBox(
-        width: 130,
-        height: 50,
-        child: MaterialButton(
-           color:Colors.white,
-
-            shape: RoundedRectangleBorder(
-                borderRadius:
-                BorderRadius.all(Radius.circular(25)),
-                side: BorderSide(
-                    width: 2, color: Color(0xffFE6C00))),
-            onPressed: () {
-              Navigator.push(context  , MaterialPageRoute(builder:(context )=>Login()));
-              },
-
-            child:
-                Text("Sign In",
-                    style: TextStyle(
-                      fontSize: 15,
-color:Color(0xffFE6C00)
-                    )),
-
-
-            )),
-      SizedBox(
-          width: 130,
-          height: 50,
-          child: MaterialButton(
-
-              color: Color(0xffFE6C00),
-            shape: RoundedRectangleBorder(
-                borderRadius:
-                BorderRadius.all(Radius.circular(25)),
-                side: BorderSide(
-                    width: 2, color: Color(0xffFE6C00))),
-            onPressed: () {
-              Navigator.push(context  , MaterialPageRoute(builder:(context )=>SignUp()));
-            },
-
-            child:
-            Text("Sign Up",
-                style: TextStyle(
-                    fontSize: 15,
-                    color:Colors.white
-                )),
-
-
-          )),
-
-
-
-
-
-
-    ]
-)
-
-))
-]
-
-
-
-
-
-          )
-
-
-
-
-
-
-
-          )
-
-
-
-
-
-
-
+              children: [
+                SizedBox(height: 50),
+                const Text(
+                  "Welcome",
+                  style: TextStyle(
+                      color: Color(0xff333333),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: 365,
+                  height: 108,
+                  child: Column(
+                    children: [
+                      const Text(
+                        "ToDo helps you organize \nyour day and achive your \ngoals effortlessly. ",
+                        style: TextStyle(
+                          color: Color(0xff707070),
+                          fontSize: 18.5,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Home()),
+                            );
+                          },
+                          icon: Icon(
+                            Icons.arrow_circle_right,
+                            size: 80,
+                            color: Color(0xffFE6C00),
+                          ),
+                        ),
+                        /*
+                        SizedBox(
+                          width: 130,
+                          height: 50,
+                          child: MaterialButton(
+                            color: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(25)),
+                                side: BorderSide(
+                                    width: 2, color: Color(0xffFE6C00))),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Login()));
+                            },
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  fontSize: 15, color: Color(0xffFE6C00)),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 130,
+                          height: 50,
+                          child: MaterialButton(
+                            color: Color(0xffFE6C00),
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(25)),
+                                side: BorderSide(
+                                    width: 2, color: Color(0xffFE6C00))),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignUp()));
+                            },
+                            child: Text(
+                              "Sign Up",
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        */
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
-
-
-
-
-
-      )
-
-
-
-
-
-
-
-
+      ),
     );
   }
 }
